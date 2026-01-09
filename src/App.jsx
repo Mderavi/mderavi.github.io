2e15c0ecb244e635564d9aa3421867e912d2759a
Mderavi <deravi.mahsa@gmail.com>
2026-01-09 17:02:08 +1100

    Update portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,35 @@
+<<<<<<< HEAD
 import logo from './logo.svg';
 import './App.scss';
 import Navbar from './components/Navbar/Navbar';
 import Hero from './components/Hero/Hero';
 import Skills from './components/Skills/Skills';
+=======
+import logo from "./logo.svg";
+import "./App.scss";
+import Navbar from "./components/Navbar/Navbar";
+import Hero from "./components/Hero/Hero";
+import Skills from "./components/Skills/Skills";
+import WorkExperience from "./components/WorkExperience/WorkExperience";
+>>>>>>> 94f5042 (update css for color)
 
 function App() {
   return (
     <>
+<<<<<<< HEAD
     <Navbar />
     <div className="container">
      <Hero />
      <Skills />
     </div>
+=======
+      <Navbar />
+      <div className="container">
+        <Hero />
+        <Skills />
+        <WorkExperience />
+      </div>
+>>>>>>> 94f5042 (update css for color)
     </>
   );
 }
